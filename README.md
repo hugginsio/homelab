dd864ddd81493d062c2328552427097304736eff
Kyle Huggins <kyle@huggins.io>
2023-07-28 17:53:19 -0500

    Reorganize files, add landing page (#16)
    * feat!: reorganize files and update env variables
    
    All `docker-compose` files have been reorganized based on how many
    instances I typically run and moved to a single `docker/` subdirectory.
    This will allow me to better organize my homelab materials as I try
    other technologies (such as Kubernetes and Nomad).
    
    * feat: add external-landing
    
    * feat(external-landing): add favicon
    
    * feat(external-landing): add wrangler deploy script
    
    * feat: add editorconfig
    
    * feat: add dependabot.yml
    
    * feat(external-landing): add cicd workflows
    
    * docs: add README to repository root
    
    * feat(external-landing): update deployment action
    
    * feat(actions): add default working directory
    
    * docs(docker): add README
    
    * feat(docker): add changedetectionio

---
+++ README.md
@@ -0,0 +1,15 @@
+# homelab
+
+This repository is the "production-ready" version of my homelab environment.
+
+## Structure
+
+```text
+.
+├── docker            # Docker Compose files and supporting documentation
+│   ├── multi         # Containers ran on multiple nodes (e.g. Adguard Home)
+│   └── single        # Containers ran on a single node (e.g. Paperless NGX)
+└── external-landing  # The external landing page for non-VPN users
+```
+
+Additional documentation is present in each module's directory.
---
+++ external-landing/app/layout.tsx
@@ -0,0 +1,26 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Huggins Cloud",
+  description:
+    "Trying to access the Huggins Cloud? Make sure your VPN is enabled and connected.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html
+      lang="en"
+      className="bg-neutral-50 text-neutral-950 dark:bg-neutral-950 dark:text-neutral-50"
+    >
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
